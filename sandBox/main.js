4a18ad2f6db71c0f18f1f1685d10f907ca90b229
Sandbox Sync <support@getsandbox.com>
2015-10-06 08:55:11 -0400

    Changes

---
+++ sandBox/main.js
@@ -0,0 +1,106 @@
+/**
+ * SandBox API for the sample TargetRest Service
+ * 
+ */
+
+Sandbox.define('/api/cards/v1/{paymentToken}','PUT', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    // Set the type of response, sets the Content-Type header.
+    res.type('application/json');
+    
+    //getting the payment token from the request parameter
+    var paymentToken = req.params.paymentToken;
+       
+    /* ************** getting the values from the request body ************** */
+    
+	var accountType = req.body.accountTypeURI;
+    var networkType = req.body.networkTypeURI;
+    var walletId = req.body.walletId;
+    var customerId = req.body.customerId;
+    var teamMemberNumber;
+    var cartwheelNumber;
+    
+    console.log("paymentToken: " + paymentToken);
+    console.log("accountType: " + accountType);
+    console.log("networkType: " + networkType);
+    console.log("walletId: " + walletId);
+    console.log("customerId: " + customerId);
+    
+    //card number for response
+    var cardNumber;
+    
+	//checking for the type of the add card request
+	if(accountType.trim().match(/LoyaltyCartwheel/) == "LoyaltyCartwheel"){
+		cartwheelNumber = req.body.cartwheelNumber;
+        cardNumber = cartwheelNumber.substr(cartwheelNumber.length - 4);
+        console.log("cartwheel #: " + cartwheelNumber + " card #: " + cardNumber);
+    }
+    
+    else{
+        teamMemberNumber = req.body.teamMemberNumber;
+        cardNumber = teamMemberNumber.substr(teamMemberNumber.length - 4);
+        console.log("teamMember #: " + teamMemberNumber + " card #: " + cardNumber);
+    }
+    
+    /* ************** generating the respond ************** */
+
+    // Set the status code of a successful response.
+    res.status(200);
+    
+    // Response body for a cartwheel request
+    if(cartwheelNumber !== undefined){
+        res.json({
+            "href": "http://targettest.getsandbox.com" + req.url,
+            "accountTypeURI": accountType,
+            "networkTypeURI": networkType,
+            "walletId": walletId,
+            "customerId": customerId,
+            "cartwheelNumber": cardNumber,
+            "paymentToken": paymentToken,
+            "cardType": "60", 
+            "actionStatus": "1", 
+            "actionMessage": "Success"
+        });
+    }
+    
+    // Response body for a teamMember request
+    else{
+        res.json({
+            "href": "http://targettest.getsandbox.com" + req.url,
+            "accountTypeURI": accountType,
+            "networkTypeURI": networkType,
+            "walletId": walletId,
+            "customerId": customerId,
+            "teamMemberNumber": cardNumber,
+            "paymentToken": paymentToken,
+            "cardType": "70", 
+            "actionStatus": "1", 
+            "actionMessage": "Success"
+        });
+    }
+});
+
+Sandbox.define('/api/cards/v1/{paymentToken}','DELETE', function(req, res) {
+	
+    //checking the request to see if it's in the proper format
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    //setting the type of response, sets the Content-Type header.
+    res.type('application/json');
+    
+    //setting the status code for a successful response.
+    res.status(200);
+    
+    //send the response body.
+    res.json({
+        "status": "ok",
+		"actionStatus": "1",
+		"actionMessage": "Success"
+    });
+});
