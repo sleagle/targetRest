dc1acab8dd4601477298e182ea4285fa3a06aec8
Sandbox Sync <support@getsandbox.com>
2015-10-07 05:36:40 -0400

    Changes

--- sandBox/main.js
+++ sandBox/main.js
@@ -109,7 +109,7 @@ Sandbox.define('/api/cards/v1/{paymentToken}','DELETE', function(req, res) {
     var paymentToken = req.params.paymentToken;
     console.log("index of null: " + paymentToken.trim().indexOf("null"));
     
-	if(paymentToken.trim().indexOf('null') !== 0 || paymentToken !== ""){
+	if(paymentToken.trim().indexOf("null") !== 0 || paymentToken !== ""){
 			
 		if(paymentToken.trim().indexOf('4') > -1){
 
